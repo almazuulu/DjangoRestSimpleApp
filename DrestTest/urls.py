934f43466b712cbafc95fc8225ba0ee6e688ea5e
almazuulu <almazuulu@gmail.com>
2022-05-09 06:24:10 +0600

    Created new model and view

--- DrestTest/urls.py
+++ DrestTest/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from woman.views import WomenAPIView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/womenlist/',WomenAPIView.as_view())
+
 ]
